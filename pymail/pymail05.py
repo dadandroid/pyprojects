917d77383c03dd6d5ab865c4c38a8957adaa9c30
dadandroid <davidandresbcn@gmail.com>
2017-11-19 19:00:38 +0100

    -testmail

--- pymail/pymail05.py
+++ pymail/pymail05.py
@@ -1,8 +1,4 @@
 import smtplib
-#from email.MIMEMultipart import MIMEMultipart
-#from email.MIMEText import MIMEText
-#from email.MIMEBase import MIMEBase
-#from email import encoders
 import email
 import imp
 import os
