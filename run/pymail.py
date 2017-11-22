9cc45ac47a14cfdc80a00cab66e68ca1a0438dc2
dadandroid <davidandresbcn@gmail.com>
2017-11-22 21:46:23 +0100

    -backup done

--- run/pymail.py
+++
@@ -1,9 +0,0 @@
-import smtplib
- 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login("ojeto54@gmail.com", "CosmoCarretero0604")
- 
-msg = "TEST MESSAGE"
-server.sendmail("ojeto54@gmail.com", "davidandresbcn@gmail.com", msg)
-server.quit()
